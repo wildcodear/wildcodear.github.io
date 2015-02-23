b310ff473bd5584aa9c1aca4023b0cbaf04e15d4
diegoduncan21 <diegoduncan21@gmail.com>
2015-02-22 23:47:27 -0300

    #7 cambio de imagenes en el primer slide

--- js/custom.js
+++ js/custom.js
@@ -2,9 +2,34 @@
 
 	new WOW().init();
 
-	jQuery(window).load(function() { 
+	jQuery(window).load(function() {
 		jQuery("#preloader").delay(100).fadeOut("slow");
 		jQuery("#load").delay(100).fadeOut("slow");
+
+		// Cambiar imagenes del primer slide
+	    var slide = $('.intro');
+	    var backgrounds = [
+	      'url(img/slides/1.jpg) no-repeat',
+	      'url(img/slides/2.jpg) no-repeat',
+	      'url(img/slides/3.jpg) no-repeat',
+	      'url(img/slides/4.jpg) no-repeat',
+	      'url(img/slides/5.jpg) no-repeat',
+	    ];
+	    var current = 0;
+
+	    function nextBackground() {
+	        slide.animate({opacity: 0.25}, 'slow', function() {
+		        $(this)
+		            .css(
+			            'background',
+			        backgrounds[current = ++current % backgrounds.length])
+		            .animate({opacity: 1});
+		    });
+	        setTimeout(nextBackground, 5000);
+	    }
+	    setTimeout(nextBackground, 5000);
+	    slide.css('background', backgrounds[0]);
+
 	});
 
 
